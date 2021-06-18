9385a3358392160bc8d7b390294de3a15d230a17
Syafiqjos <syafiqjos@gmail.com>
2021-06-18 10:24:46 +0700

    make main socket preparation connection

--- database/program_database.c
+++ database/program_database.c
@@ -12,53 +12,24 @@
 
 #define PORT 8080
 
-int amain(int argc, char const *argv[]) {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-      
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-      
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-      
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    return 0;
-}
-
 char *databases_path = "databases";
 int buffersize = 1024;
 char filebuffer[1024];
+
+int server_fd, new_socket, valread;
+struct sockaddr_in address;
+int opt = 1;
+int addrlen = sizeof(address);
+char sendbuffer[1024] = {0};
+char receivebuffer[1024] = {0};
+
+void receive_message(){	
+    	valread = read( new_socket , receivebuffer, buffersize);
+}
+
+void send_message(char *message){
+	send(new_socket , message , buffersize, 0 );
+}
 
 bool make_directory(char *path){
 	struct stat st = {0};
@@ -591,13 +562,13 @@ int check_input (){
 	char input[1024];
 	scanf("%s", input);
 	if (create_user_input(input)){
-
+		
 	}
 	else if(use_database_input(input)){
-	
+		
 	}
 	else if(grant_permission_input(input)){
-	
+		
 	}
 	else if(create_input(input)){
 		
@@ -609,10 +580,10 @@ int check_input (){
 		
 	}
 	else if(update_table_input(input)){
-
+		
 	}
 	else if(delete_table_input(input)){
-
+		
 	}
 	else if(select_table_input(input)){
 		
@@ -621,7 +592,7 @@ int check_input (){
 	return 0;
 }
 
-int main(int argc, const char *argv[]) {
+int initial() {
 	make_directory(databases_path);
 	make_database("__ROOT__");
 	make_table("__ROOT__", "__USER__"); //user login password relation
@@ -632,3 +603,43 @@ int main(int argc, const char *argv[]) {
 
 	create_user_input("CREATE USER gajah IDENTIFIED BY gajahjuga");
 }
+
+void prepare_socket(){	
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+	prepare_socket();
+
+	initial();
+
+	return 0;
+}
+
